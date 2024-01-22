bfe25e245ffa520dc0b3e027241baf7e2c5cb334
Paul Wong <paul@jaiken.com>
2024-01-22 11:24:11 -0800

    update terraform for express

---
+++ modules/express-example/variables.tf
@@ -0,0 +1,30 @@
+#---------------------------------------------------------------------------------------------------
+# Express API
+#---------------------------------------------------------------------------------------------------
+variable "module_name" {
+  sensitive   = false
+  type        = string
+  description = "Kubernetes Module Name"
+  default     = "express-example"
+}
+
+variable "domain_name" {
+  sensitive   = false
+  type        = string
+  description = "Domain Name"
+  default     = "example.com"
+}
+
+variable "subdomain_name" {
+  sensitive   = false
+  type        = string
+  description = "Subdomain Name for Ingress"
+  default     = "express"
+}
+
+variable "host_data_directory" {
+  sensitive   = false
+  type        = string
+  description = "Host Data Directory"
+  default     = "/shares/data"
+}
